f87eae13725ad6e849099acfc03e48e3463a9c5b
Pedro Henrique Moreira <63320967+PedroMoreira87@users.noreply.github.com>
2021-01-27 18:15:56 -0300

    Delete Geek Exercícios directory

--- Geek Exercícios/src/secao03/E02.java
+++
@@ -1,9 +0,0 @@
-package secao03;
-
-public class E02 {
-	
-	public static void main(String[] args) {
-		float num = 1.0f;
-		System.out.println(num);
-	}
-}
--- Geek Exercícios/src/secao03/E11.java
+++
@@ -1,9 +0,0 @@
-package secao03;
-
-public class E11 {
-	public static void main(String[] args) {
-		float ms = 33.33f;
-		float kmh = ms * 3.6f;
-		System.out.println(kmh);
-	}
-}
